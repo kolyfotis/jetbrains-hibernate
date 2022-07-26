bb98f0a6840ffffa329a1c62c24a16d3f2795422
AKKA\Fotios.Kolytoumpas <fotis.kolytoumpas@gmail.com>
2022-07-26 11:00:55 +0200

    Test Apache Shiro authentication

---
+++ src/main/java/com/example/resources/HelloShiro.java
@@ -0,0 +1,23 @@
+package com.example.resources;
+
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.authz.annotation.RequiresUser;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("shiro")
+public class HelloShiro {
+    @GET
+    @RequiresUser
+    public String sayHelloShiro() {
+        return "Hello!";
+    }
+
+    @GET
+    @Path("define")
+    @RequiresPermissions("hello:define")
+    public String defineShiro() {
+        return  "Shiro is the Japanese term for a castle";
+    }
+}
