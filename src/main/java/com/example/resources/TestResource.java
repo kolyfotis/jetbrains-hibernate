6a7435e0bdfd69ebc26232fbbc937a0a76ae1a1d
AKKA\Fotios.Kolytoumpas <fotis.kolytoumpas@gmail.com>
2022-07-26 13:32:30 +0200

    Custom Annotation for User Authentication

--- src/main/java/com/example/resources/TestResource.java
+++ src/main/java/com/example/resources/TestResource.java
@@ -1,5 +1,7 @@
 package com.example.resources;
 
+import com.example.filters.UserAuthentication;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
@@ -7,6 +9,7 @@ import javax.ws.rs.core.MediaType;
 public class TestResource {
 
     @GET
+    @UserAuthentication
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
         return "HI FROM TEST RESOURCE";
