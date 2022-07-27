a8fa145310bd1c41c39ea87b44eedeb4e138b768
AKKA\Fotios.Kolytoumpas <fotis.kolytoumpas@gmail.com>
2022-07-27 10:48:40 +0200

    Stored authorized roles in a List to simplify if condition

--- src/main/java/com/example/filters/EmployeeAuthorizationFilter.java
+++ src/main/java/com/example/filters/EmployeeAuthorizationFilter.java
@@ -7,6 +7,8 @@ import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 @EmployeeAuthorization
@@ -15,10 +17,10 @@ public class EmployeeAuthorizationFilter implements ContainerRequestFilter {
 
     @Override
     public void filter(ContainerRequestContext containerRequestContext) throws IOException {
+        final List<String> authorizedRoles = Arrays.asList("admin", "employee");
+
         User retrievedUser = HelperMethods.getUserByHeaders(containerRequestContext);
-        if (Objects.nonNull(retrievedUser) &&
-                (retrievedUser.getRole().equals("admin") ||
-                        retrievedUser.getRole().equals("employee"))) {
+        if (Objects.nonNull(retrievedUser) && authorizedRoles.contains(retrievedUser.getRole())) {
             return;
         }
         Response unauthorizedResponse = Response
