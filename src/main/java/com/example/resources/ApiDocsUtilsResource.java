b9190d14e67c9924ee1b5725af49614231c05a1a
kolyfotis <fotis.kolytoumpas@gmail.com>
2022-08-17 11:07:48 +0200

    Endpoints to export API doc to JSON and YAML

--- src/main/java/com/example/resources/ApiDocsUtilsResource.java
+++ src/main/java/com/example/resources/ApiDocsUtilsResource.java
@@ -2,53 +2,81 @@ package com.example.resources;
 
 import com.example.model.Message;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 @Path("utils")
 public class ApiDocsUtilsResource {
-    private static FileWriter fileWriter;
+
+    private enum FileType { JSON, YAML }
 
     @GET
     @Path("generateApiDocJSON")
-    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
     public Response generateAPIDocumentationJSON() {
 
         Client webClient = ClientBuilder.newClient();
 
+        // retrieve api doc from openapi url
         Response res = webClient.target("http://localhost:8080/jetbrains-hibernate")
                 .path("openapi")
                 .request()
                 .accept(MediaType.APPLICATION_JSON_TYPE)
                 .get();
 
-//        System.out.println("\t=====");
-//        System.out.println(res.readEntity(String.class));
-//        System.out.println("\t=====");
-
         // save response to file
-        saveJSONFile(res);
+        saveFile(res, FileType.JSON);
 
         return Response.status(200)
-                .entity(new Message("Api Documentation file successfully created"))
+                .entity(new Message("Api Documentation file successfully created in " +
+                        "\"jetbrains-hibernate\\api-docs\" directory."))
                 .build();
     }
 
-    private void saveJSONFile(Response res) {
+    @GET
+    @Path("generateApiDocYAML")
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public Response generateAPIDocumentationYAML() {
+
+        Client webClient = ClientBuilder.newClient();
+
+        // retrieve api doc from openapi url
+        Response res = webClient.target("http://localhost:8080/jetbrains-hibernate")
+                .path("openapi")
+                .request()
+                .accept("application/yaml")
+                .get();
+
+        // save response to file
+        saveFile(res, FileType.YAML);
+
+        return Response.status(200)
+                .entity(new Message("Api Documentation file successfully created in " +
+                        "\"jetbrains-hibernate\\api-docs\" directory."))
+                .build();
+    }
+
+    private void saveFile(Response res, FileType fileType) {
+
+        String pathToApiDocsDir = "C:\\Users\\Fotios.Kolytoumpas\\IdeaProjects\\jetbrains-hibernate\\api-docs\\";
+        FileOutputStream out = null;
 
         try {
-            FileOutputStream out = new FileOutputStream("C:\\Users\\Fotios.Kolytoumpas\\IdeaProjects\\jetbrains-hibernate\\api-docs\\ApiDocumentation.json");
-
-            System.out.println("File opened/created.");
+            switch (fileType) {
+                case JSON:
+                    out = new FileOutputStream(pathToApiDocsDir + "ApiDocumentation.json");
+                    break;
+                case YAML:
+                    out = new FileOutputStream(pathToApiDocsDir + "ApiDocumentation.yaml");
+                    break;
+            }
+            System.out.println("File accessed.");
             try {
                 out.write(res.readEntity(String.class).getBytes());
                 System.out.println("Data written.");
@@ -60,21 +88,5 @@ public class ApiDocsUtilsResource {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
-//        try {
-//            fileWriter = new FileWriter("ApiDocumentation.json");
-//            fileWriter.write(res.readEntity(String.class));
-//
-//            System.out.println("ApiDocumentation.json successfully persisted.");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } finally {
-//            try {
-//                fileWriter.flush();
-//                fileWriter.close();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
     }
 }
